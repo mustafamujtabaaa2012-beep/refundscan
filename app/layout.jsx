7620fe797b2ae740d4081f28c8f26d7c9118668b
mustafamujtabaaa2012-beep <mustafamujtabaaa2012@gmail.com>
2026-05-04 19:23:00 +0500

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Refund Scan',
+  description: 'Amazon Inventory Analytics',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
